12d1239c9889148aa288761c9ace14b466a69b6c
ganghyeon <vmfpel3@gmail.com>
2023-09-14 12:30:21 +0900

    스클

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-let button = document.querySelector("input")
+document.addEventListener("DOMContentLoaded",
+    function(e){
+        let a = document.querySelector("input")
+        a.addEventListener("input", 
+            function(e){
+                console.log(e.target.value)
+            }
+        )
+    }
+)
+function hi(){
+    alert("hi")
+}
+
