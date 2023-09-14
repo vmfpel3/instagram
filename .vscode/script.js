6d92ce790d4219326b7636b916d3542e4539f0aa
ganghyeon <vmfpel3@gmail.com>
2023-09-14 16:27:24 +0900

    id 바꾸기

---
+++ .vscode/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded",
+    function(e){
+        let configID = document.querySelector("#id i")
+        let idText = document.querySelector("#id span")
+
+        configID.addEventListener("click",
+            function(e){
+                idText.textContent = prompt("새로운 아이디를 입력하세요")
+            }
+        )
+    }
+)
